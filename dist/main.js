a851949a0d292ba04edca424c859bfcc894c9beb
mohseenrm <mohseenmukaddam6@gmail.com>
2017-01-02 00:28:47 -0700

    adding scroll code

--- dist/main.js
+++ dist/main.js
@@ -8,4 +8,15 @@
     var scene = document.getElementById('scene');
     var parallax = new Parallax(scene);
 })();
+
+$(document).ready(function () {
+    $(window).bind('scroll', function () {
+        var navHeight = $(window).height() - 70;
+        if ($(window).scrollTop() > navHeight) {
+            $('nav').addClass('fixed');
+        } else {
+            $('nav').removeClass('fixed');
+        }
+    });
+});
 //# sourceMappingURL=main.js.map
--- js/main.js
+++ js/main.js
@@ -8,3 +8,15 @@
     const scene = document.getElementById('scene');
     const parallax = new Parallax(scene);
 })();
+
+$(document).ready(function(){
+    $(window).bind('scroll', function() {
+    var navHeight = $( window ).height() - 70;
+            if ($(window).scrollTop() > navHeight) {
+                $('nav').addClass('fixed');
+            }
+            else {
+                $('nav').removeClass('fixed');
+            }
+    });
+});
