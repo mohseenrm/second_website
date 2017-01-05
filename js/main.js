5ff60703816568021449330da2ad1d9bc878493e
mohseenrm <mohseenmukaddam6@gmail.com>
2017-01-04 21:22:25 -0700

    vanilla JS funcs complete

--- js/main.js
+++ js/main.js
@@ -10,6 +10,26 @@
 })();
 
 (()=>{
+
+    const addClass = ( element, newClassName ) => {
+        if( element !== undefined && newClassName !== undefined ){
+            let classNames = element.className;
+            if( !classNames.includes( newClassName ) ){
+                element.className += ` ${newClassName}`;
+            }
+        }
+        return;
+    };
+
+    const removeClass = ( element, delClassName ) => {
+        if( element !== undefined && delClassName !== undefined ){
+            if( element.className.includes( delClassName ) ){
+                element.className = element.className.replace( new RegExp('(?:^|\\s)'+ delClassName + '(?:\\s|$)'), '' );
+            }
+        }
+        return;
+    };
+
     window.onscroll = ()=>{
         let navHeight = window.innerHeight - 70;
         let nav = document.getElementById("nav-bar");
@@ -22,7 +42,10 @@
             nav.className = "";
         
         if( window.scrollY > 400 )
-            document.querySelector('.visible').className += " animated slideInUp";
+            // document.querySelector('.visible').className += " animated slideInUp";
+            addClass( document.querySelector( '.visible' ), 'animated slideInUp' );
+        else
+            removeClass( document.querySelector( '.visible' ), 'animated slideInUp' );
     };
 })();
 
