5dc9195f62a57f2a10c6589444b412abc45929ff
mohseenrm <mohseenmukaddam6@gmail.com>
2017-08-16 18:46:06 -0700

    updating entry script

--- src/js/main.js
+++ src/js/main.js
@@ -41,64 +41,6 @@ THE SOFTWARE.
 	console.log( `hello ${ me }!` );
 } )();
 
-( () => {
-    // /**
-    // * @function addClass
-    // * @param  {element} element      Element whose class name needs to be altered
-    // * @param  {String } newClassName Class Name
-    // * @return None
-    // */
-    // const addClass = ( element, newClassName ) => {
-    //     if( element !== undefined && newClassName !== undefined ){
-    //         let classNames = element.className;
-    //         if( !classNames.includes( newClassName ) ){
-    //             element.className += ` ${newClassName}`;
-    //         }
-    //     }
-    //     return;
-    // };
-
-    // /**
-    // * @function removeClass
-    // * @param  {element} element      Element whose class name needs to be altered
-    // * @param  {String } delClassName Class Name
-    // * @return None
-    // */
-    // const removeClass = ( element, delClassName ) => {
-    //     if( element !== undefined && delClassName !== undefined ){
-    //         if( element.className.includes( delClassName ) ){
-    //             element.className = element.className.replace( new RegExp('(?:^|\\s)'+ delClassName + '(?:\\s|$)'), '' );
-    //         }
-    //     }
-    //     return;
-    // };
-
-    //http://codepen.io/madebyjam/pen/eBpegv
-    //Animation on Scroll: https://github.com/michalsnik/aos
-    //Pie Chart: http://codepen.io/Siddharth11/pen/LVQmjN
-
-    // window.onscroll = ()=>{
-    //     const win = window;
-
-    //     //titles reveal
-    //     if( win.scrollY > 400 )
-    //         // document.querySelector('.visible').className += " animated slideInUp";
-    //         addClass( document.querySelector( '.visible' ), 'animated slideInUp' );
-    //     else
-    //         removeClass( document.querySelector( '.visible' ), 'animated slideInUp' );
-
-    //     if( win.scrollY > ( 0.12 * win.innerHeight ) ){
-    //         document.getElementById( 'first-header' ).style.visibility = 'visible';
-    //         addClass( document.getElementById( 'first-header' ), 'animated zoomInDown' );
-    //     }
-
-    //     else
-    //         removeClass( document.getElementById( 'first-header' ), 'animated zoomInDown' );
-
-    // };
-
-} )();
-// TODO: generate prefix
 require( './scroll' );
 require( './navigation' );
 require( './panels' );
